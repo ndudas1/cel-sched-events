90c971f540cc4f69d2764d237b78e525301e4072
Nick Dudas <nicholas.dudas@gmail.com>
2025-04-08 20:37:11 -0600

    remove init data

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -8,6 +8,3 @@ CREATE TABLE IF NOT EXISTS events (
     end_time TIMESTAMP WITH TIME ZONE NOT NULL,
     frequency INT DEFAULT NULL
 );
-
-Insert INTO events (name, email, notes, start_time, end_time, frequency)
- Values ('nick', 'email', 'no notes', '2025-10-01T22:00:00-07:00', '2025-10-01T23:00:00-07:00', 2);
